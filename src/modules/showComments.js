6047c72ad3126e1b1e652119b16e971afc456d2b
toyman640 <falakoadebayor@gmail.com>
2023-06-02 15:15:41 +0100

    comment

--- src/modules/showComments.js
+++ src/modules/showComments.js
@@ -1,13 +1,18 @@
+import keepComment from './addcomment.js';
+import getComment from './getcomment.js';
+
 const popCard = document.querySelector('.pop-card');
+const mealsDiv = document.querySelector('.meals');
 
 const showPop = (buttonId) => {
   fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${buttonId}`)
     .then((res) => res.json())
     .then((data) => {
       popCard.innerHTML = `
-      <div class="">
-    <img class="close" onclick="this.parentNode.style.display = 'none';" src="https://static.vecteezy.com/system/resources/thumbnails/020/048/793/small/x-transparent-background-free-png.png">
+      <div class="card">
+      
       <div class="popup-container">
+      <button class="closeBtn"><img class="close" src="https://static.vecteezy.com/system/resources/thumbnails/020/048/793/small/x-transparent-background-free-png.png"></button>
       <img class="popImg" src="${data.meals[0].strMealThumb}">
       <h3 class="FoodName">${data.meals[0].strMeal}</h3>
       <p class="ingredients">
@@ -22,21 +27,6 @@ const showPop = (buttonId) => {
       ${data.meals[0].strIngredient6},
       ${data.meals[0].strIngredient7},
       ${data.meals[0].strIngredient8},
-      ${data.meals[0].strIngredient9},
-      ${data.meals[0].strIngredient10},
-      ${data.meals[0].strIngredient11},
-      ${data.meals[0].strIngredient12},
-      ${data.meals[0].strIngredient13},
-      ${data.meals[0].strIngredient14},
-      ${data.meals[0].strIngredient15},
-      ${data.meals[0].strIngredient16}
-      ${data.meals[0].strIngredient17}
-      ${data.meals[0].strIngredient18}
-      ${data.meals[0].strIngredient19}
-      ${data.meals[0].strIngredient20}
-      </div>
-      <div class="commentTitle">
-      &nbsp;Comments&nbsp;[<span class="commentC">0</span>]
       </div>
       <div class="comments">
       </div><br>
@@ -44,16 +34,31 @@ const showPop = (buttonId) => {
       <div class="commentTitle" align="center">
       Add comment
       </div>
-      <input type="text" class="username" placeholder="username"><br>
+      <input type="hidden" id="commentId" class="username" value="${data.meals[0].idMeal}"><br>
+      <input type="text" id="name" class="username" placeholder="username"><br>
       <textarea id="msg" name="user_message" placeholder="Your insight" required maxlength="500"></textarea><br>
-      <button class="submit" type="button">Comment</button>
+      <button id="commentBtn" class="submit" type="button">Comment</button>
       <div class="idMeal">${data.meals[0].idMeal}</div>
       </form>
       </div>
       </div>
       `;
+      const closeBtn = document.querySelector('.closeBtn');
+      const closePop = () => {
+        popCard.classList.toggle('pop-card');
+        mealsDiv.classList.toggle('blur');
+      };
+      closeBtn.addEventListener('click', closePop);
+      const commentButton = document.getElementById('commentBtn');
+      commentButton.addEventListener('click', async () => {
+        keepComment();
+        setTimeout(getComment, 3000);
+      });
+      getComment();
     });
+
   popCard.classList.toggle('pop-card');
+  mealsDiv.classList.toggle('blur');
 };
 
 const checkbtn = (event) => {
